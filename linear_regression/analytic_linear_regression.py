85d6fa8824b9e2d2af6f58457039fe63d7e0f62a
Shreyas-Ekanathan <shreyase39@gmail.com>
2026-05-06 21:34:49 -0400

    analytic works

--- linear_regression/analytic_linear_regression.py
+++ linear_regression/analytic_linear_regression.py
@@ -2,13 +2,13 @@
 import numpy as np
 
 def generate_data():
-    slopes = np.random.uniform(-204004.96, 1204021490124.44, 64)
-    intercept = np.random.uniform(-12049024.24, 1409204.24)
+    slopes = np.random.uniform(-1.0, 1.0, 64)
+    intercept = np.random.uniform(-1.0, 1.0)
     x_vals = np.zeros((10000, 64))
     y_vals = np.zeros(10000)
     for i in range(10000):
         x = np.random.uniform(-12049109402.4, 04192091049.4, 64)
-        y = np.dot(x, slopes) + intercept + np.random.uniform(-0.0001, 0.00001)
+        y = np.dot(x, slopes) + intercept + np.random.uniform(-0.001, 0.001)
         x_vals[i] = x
         y_vals[i] = y
     return x_vals, y_vals, slopes, intercept
@@ -25,4 +25,6 @@ A_inv = np.linalg.inv(A)
 b = xT @ data_y
 weights = A_inv @ b
 true_weights = np.append(true_slope, true_intercept)
-print(np.linalg.norm(weights-true_weights, ord=None, axis=None, keepdims=False))
+print(np.linalg.norm(weights - true_weights, ord=None, axis=None, keepdims=False))
+print(weights)
+print(true_weights)
