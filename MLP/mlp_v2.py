29f473c52fd464717b312f529d67764433c1695a
Shreyas-Ekanathan <shreyase39@gmail.com>
2026-05-13 15:32:01 -0400

    prelim model stuff

--- MLP/mlp_v2.py
+++ MLP/mlp_v2.py
@@ -7,7 +7,24 @@
 
 # we will train a classifier to learn some sort of strange spiral shape or something like that
 # model architecture:
-# 2 layers
-# first layer takes input -> batch normalize -> ReLU -> second layer -> softmax -> output
+# 3 layers
+# first layer takes input -> batch normalize -> ReLU -> second layer -> batch normalize -> ReLU -> third layer -> softmax -> output
 # training: on any given iteration, we'll kill say 30% of neurons. apply ADAM and batching to train.
 # loss function will have a L2 regularizer 
+
+import numpy as np
+
+class model: 
+    def __init__(self):
+        self.w1 = np.zeros((16, 128))
+        self.b1 = np.zeros(128)
+        self.w2 = np.zeros((128, 32))
+        self.b2 = 32
+        self.w3 = np.zeros(32)
+        self.b3 = 0
+        
+    def cross_entropy_loss(prediction, target, batch_size):
+        return np.sum(-target[i] * np.log(prediction[i]) for i in range(batch_size))
+    
+    def regularized_loss(self, prediction, target, batch_size, reg_coef):
+        return self.cross_entropy_loss(prediction, target, batch_size) + reg_coef * (self.w1 ** 2 + self.w2 ** 2 + self.w3 ** 2 + self.b1 ** 2 + self.b2 ** 2 + self.b3 ** 2) 
