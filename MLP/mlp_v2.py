1ffb7194811aa0a343392d39aa4dbe3ccf4c12fb
Shreyas-Ekanathan <shreyase39@gmail.com>
2026-05-14 16:39:02 -0400

    starting backprop

--- MLP/mlp_v2.py
+++ MLP/mlp_v2.py
@@ -16,100 +16,111 @@ import numpy as np
 
 class model: 
     def __init__(self):
+        #TODO: implement an actual initialization scheme
         self.w1 = np.zeros((16, 128))
-        self.w1_m1_t0 = np.zeros((16, 128))
-        self.w1_v1_t0 = np.zeros((16, 128))
-        self.w1_m1_t1 = np.zeros((16, 128))
-        self.w1_v1_t1 = np.zeros((16, 128))
-        self.b1 = np.zeros(128)
-        self.b1_m1_t0 = np.zeros(128)
-        self.b1_v1_t0 = np.zeros(128)
-        self.b1_m1_t1 = np.zeros(128)
-        self.b1_v1_t1 = np.zeros(128)
-        self.w2 = np.zeros((128, 32))
-        self.w2_m2_t0 = np.zeros((128, 32))
-        self.w2_v2_t0 = np.zeros((128, 32))
-        self.w2_m2_t1 = np.zeros((128, 32))
-        self.w2_v2_t1 = np.zeros((128, 32))
-        self.b2 = 32
-        self.b2_m2_t0 = np.zeros(32)
-        self.b2_v2_t0 = np.zeros(32)
-        self.b2_m2_t1 = np.zeros(32)
-        self.b2_v2_t1 = np.zeros(32)
-        self.w3 = np.zeros(32)
-        self.w3_m3_t0 = np.zeros(32)
-        self.w3_v3_t0 = np.zeros(32)
-        self.w3_m3_t1 = np.zeros(32)
-        self.w3_v3_t1 = np.zeros(32)
-        self.b3 = 0
-        self.b3_m3_t0 = 0
-        self.b3_v3_t0 = 0
-        self.b3_m3_t1 = 0
-        self.b3_v3_t1 = 0
+        self.w1_m = np.zeros((16, 128))
+        self.w1_v = np.zeros((16, 128))
         
-        self.gamma1 = 0
-        self.gamma2 = 0
+        self.b1 = np.zeros(128)
+        self.b1_m = np.zeros(128)
+        self.b1_v = np.zeros(128)
+        
+        self.w2 = np.zeros((128, 32))
+        self.w2_m = np.zeros((128, 32))
+        self.w2_v = np.zeros((128, 32))
+        
+        self.b2 = np.zeros(32)
+        self.b2_m = np.zeros(32)
+        self.b2_v = np.zeros(32)
+        
+        self.w3 = np.zeros((32, 10))
+        self.w3_m = np.zeros((32, 10))
+        self.w3_v = np.zeros((32, 10))
+        
+        self.b3 = np.zeros(10)
+        self.b3_m = np.zeros(10)
+        self.b3_v = np.zeros(10)
+        
+        self.gamma1 = np.ones(128) 
+        self.gamma2 = np.ones(32)
         self.beta1 = 0
         self.beta2 = 0
         
-    def cross_entropy_loss(prediction, target, batch_size):
-        return np.sum(-target[i] * np.log(prediction[i]) for i in range(batch_size))
+    def cross_entropy_loss(prediction, target):
+        return np.mean(np.sum(-target * np.log(prediction + 1e-12), axis = 1))
     
-    def regularized_loss(self, prediction, target, batch_size, reg_coef):
+    def regularized_loss(self, prediction, target, reg_coef):
         return self.cross_entropy_loss(prediction, target, batch_size) + reg_coef * (self.w1 ** 2 + self.w2 ** 2 + self.w3 ** 2 + self.b1 ** 2 + self.b2 ** 2 + self.b3 ** 2) 
     
     def relu(y):
         return np.maximum(0, y)    
     
     def softmax(y):
-        #y is just a vector of numbers
-        len = y.size
-        sum = np.sum(np.exp(y[i]) for i in range(len))
-        out = np.zeros(len)
-        for i in range(len):
-            out[i] = np.exp(y[i]) / sum 
-        return out
+        y_shifted = y - np.max(y)
+        exp_y = np.exp(y_shifted)
+        return exp_y / np.sum(exp_y)    
     
     def batch_normalize(y):
-        mu = np.mean(y, axis = 1)
-        sigma = np.var(y, axis = 1)
-        
-        return 0
+        mu = np.mean(y, axis=0, keepdims=True)
+        sigma2 = np.var(y, axis=0, keepdims=True)
+        y_norm = (y - mu) / np.sqrt(sigma2 + 1e-12)
+        return y_norm, mu, sigma2
     
     def forward_pass(self, input_batch):
         y1 = input_batch @ self.w1 + self.b1 #batch_size x 128
-        norm1 = self.gamma1 * self.batch_normalize(y1) + self.beta1
-        z1 = self.relu(norm1) # batch_size x 128
-        y2 = z1 @ self.w2 + self.b2 #batch_size x 32
-        norm2 = self.gamma2 * self.batch_normalize(y2) + self.beta2
-        z2 = self.relu(norm2) # batch_size x 32
-        y3 = z2 @ self.w3 + self.b3 #batch_size x 1
+        norm1, mu1, sigma2_1 = self.batch_normalize(y1)
+        norm1_scaled = self.gamma1 * norm1 + self.beta1
+        z1 = self.relu(norm1_scaled) # batch_size x 128
+        y2 = z1 @ self.w2 + self.b2 # batch_size x 32
+        norm2, mu2, sigma2_2 = self.batch_normalize(y2)
+        norm2_scaled = self.gamma2 * norm2 + self.beta2
+        z2 = self.relu(norm2_scaled) # batch_size x 32
+        y3 = z2 @ self.w3 + self.b3 #batch_size x 10 <- one hot encoding, each row is the predictions for one sample
         prediction = self.softmax(y3)
-        return y1, norm1, z1, y2, norm2, z2, y3, prediction
+        return y1, norm1, mu1, sigma2_1, norm1_scaled, z1, y2, norm2, mu2, sigma2_2, norm2_scaled, z2, y3, prediction
             
-    def backprop(self, input_batch, y1, ):
-        return 0, 0, 0, 0, 0, 0 #TODO
+    def backprop(self, input_batch, y1, norm1, norm1_scaled, z1, y2, norm2, norm2_scaled, z2, y3, prediction, target):
+        y3_bar = (prediction - target) / batch_size #batch_size x 10
+        w3_bar = z2.T @ y3_bar #32xbatch_size * batch_size x 10 -> 32x10
+        b3_bar = np.sum(y3_bar, axis=0)
+        w3_v_update = w3_bar ** 2
+        b3_v_update = b3_bar ** 2
+        z2_bar = y3_bar @ self.w3.T # batch_sizex10 * 10x32 -> batch_size x 32
+        norm2_scaled_bar = z2_bar * (norm2_scaled > 0)
+        gamma2_bar = np.sum(norm2 * norm2_scaled_bar, axis = 0) # vector of length 32
+        beta2_bar = np.sum(norm2_scaled_bar, axis = 0) #len 32
+        norm2_bar = self.gamma2 * norm2_scaled_bar
+        return 0
     
-    def adam(self, beta1, beta2, beta3, gamma1, gamma2, gamma3):
-        self.w1_m1_t0 = self.w1_m1_t1
-        self.b1_m1_t0 = self.b1_m1_t1
-        self.w2_m2_t0 = self.w1_m1_t1
-        self.b2_m2_t0 = self.b2_m2_t1
-        self.w3_m3_t0 = self.w3_m3_t1
-        self.b3_m3_t0 = self.b3_m3_t1
-        
-        self.w1_v1_t0 = self.w1_v1_t1
-        self.b1_v1_t0 = self.b1_v1_t1
-        self.w2_v2_t0 = self.w2_v2_t1
-        self.b2_v2_t0 = self.b2_v2_t1
-        self.w3_v3_t0 = self.w3_v3_t1
-        self.b3_v3_t0 = self.b3_v3_t1
-        
+    def adam(self, beta1, beta2, beta3, gamma1, gamma2, gamma3, alpha):
         #set desired updates with momentum
-        w1_update, b1_update, w2_update, b2_update, w3_update, b3_update = self.backprop()
-        self.w1_m1_t1 = beta1 * self.w1_m1_t0 + (1 - beta1) * w1_update
-        self.b1_m1_t1 = beta1 * self.b1_m1_t0 + (1 - beta1) * b1_update
-        self.w2_m2_t1 = beta2 * self.w2_m2_t0 + (1 - beta2) * w2_update
-        self.b2_m2_t1 = beta2 * self.b2_m2_t0 + (1 - beta2) * b2_update
-        self.w3_m3_t1 = beta3 * self.w3_m3_t0 + (1 - beta3) * w3_update
-        self.b3_m3_t1 = beta3 * self.b3_m3_t0 + (1 - beta3) * b3_update
+        (w1_m_update, b1_m_update, w2_m_update, b2_m_update, w3_m_update, b3_m_update, 
+         w1_v_update, b1_v_update, w2_v_update, b2_v_update, w3_v_update, b3_v_update,
+         gamma1_update, gamma2_update, beta1_update, beta2_update) = self.backprop()
+        self.w1_m = beta1 * self.w1_m + (1 - beta1) * w1_m_update
+        self.b1_m = beta1 * self.b1_m + (1 - beta1) * b1_m_update
+        self.w2_m = beta2 * self.w2_m + (1 - beta2) * w2_m_update
+        self.b2_m = beta2 * self.b2_m + (1 - beta2) * b2_m_update
+        self.w3_m = beta3 * self.w3_m + (1 - beta3) * w3_m_update
+        self.b3_m = beta3 * self.b3_m + (1 - beta3) * b3_m_update
+        
+        self.w1_v = gamma1 * self.w1_v + (1 - gamma1) * w1_v_update
+        self.b1_v = gamma1 * self.b1_v + (1 - gamma1) * b1_v_update
+        self.w2_v = gamma2 * self.w2_v + (1 - gamma2) * w2_v_update
+        self.b2_v = gamma2 * self.b2_v + (1 - gamma2) * b2_v_update
+        self.w3_v = gamma3 * self.w3_v + (1 - gamma3) * w3_v_update
+        self.b3_v = gamma3 * self.b3_v + (1 - gamma3) * b3_v_update
+
+        #update
+        self.w1 -= alpha * self.w1_m / (np.sqrt(self.w1_v) + 1e-12)
+        self.b1 -= alpha * self.b1_m / (np.sqrt(self.b1_m) + 1e-12)
+        self.w2 -= alpha * self.w2_m / (np.sqrt(self.w2_m) + 1e-12)
+        self.b2 -= alpha * self.b2_m / (np.sqrt(self.b2_m) + 1e-12)
+        self.w3 -= alpha * self.w3_m / (np.sqrt(self.w3_m) + 1e-12)
+        self.b3 -= alpha * self.b3_m / (np.sqrt(self.b3_m) + 1e-12)
+        self.gamma1 -= alpha * gamma1_update
+        self.gamma2 -= alpha * gamma2_update
+        self.beta1 -= alpha * beta1_update
+        self.beta2 -= alpha * beta2_update
+
+batch_size = 100
