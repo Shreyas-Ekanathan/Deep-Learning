002c9adc5b306cfd95829c65a5dae194c949c7c5
Shreyas-Ekanathan <shreyase39@gmail.com>
2026-06-20 12:42:19 -0400

    remove unnecessary imports

--- MRI2CT/cycleGAN/cycleGAN.py
+++ MRI2CT/cycleGAN/cycleGAN.py
@@ -1,11 +1,7 @@
-import numpy as np
 import torch
 import torch.nn as nn
-import torch.optim as optim
 import torch.nn.functional as F
 from torch.utils.data import DataLoader, Dataset
-import random
-import matplotlib.pyplot as plt
 from kornia.losses import ssim_loss
 import wandb
 import sys, os
